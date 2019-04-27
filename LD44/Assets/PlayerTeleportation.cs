9667788f91873e8fd0e13a7c4e7f9c58bd792c50
Clément Cornut <cornut.clement@neuf.fr>
2019-04-27 17:31:36 +0200

    Interraction

--- LD44/Assets/PlayerTeleportation.cs
+++ LD44/Assets/PlayerTeleportation.cs
@@ -8,23 +8,35 @@ public class PlayerTeleportation : MonoBehaviour
     public GameObject downTP;
 
     BoxCollider2D m_collider;
+    private bool isColliding;
+    private Collider2D objectColliding;
     // Start is called before the first frame update
     void Start()
     {
+        isColliding = false;
         m_collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isColliding) {
+            InputsPlayer iPlayer = objectColliding.GetComponent<InputsPlayer>();
+            if (iPlayer == null)
+                return;
+            if (iPlayer.interract)
+                Debug.Log("INTERRACT");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col) {
-        if(col.GetComponent<InputsPlayer>() != null)
-            Debug.Log("collide");
+        isColliding = true;
+        objectColliding = col;
     }
 
     void OnTriggerExit2D(Collider2D col) {
+        isColliding = false;
+        objectColliding = null;
         Debug.Log("stop collide");
     }
 }
