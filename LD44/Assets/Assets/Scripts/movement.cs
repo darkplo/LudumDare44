f855c6c6ad05fc1e4d8fb8b40e596c9eb2daa229
aryombre <aryombre@gmail.com>
2019-04-27 14:18:40 +0200

    Basic player movement

--- LD44/Assets/Assets/Scripts/movement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Experimental.Input;
-
-public class movement : MonoBehaviour
-{
-
-}
---
+++ LD44/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.Input;
+
+public class Movement : MonoBehaviour
+{
+	CharacterController2D charac2D;
+	float horizontalMove = 0f;
+	bool jump = false;
+	public float Speed = 40f;
+	private void Start()
+	{
+		charac2D = GetComponent<CharacterController2D>();
+	}
+	private void FixedUpdate()
+	{
+		charac2D.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+	public void Move(InputAction.CallbackContext context)
+	{
+		Vector2 mv = context.ReadValue<Vector2>();
+		mv.y = 0;
+		horizontalMove = mv.x * Speed;
+		
+	}
+	public void Jump(InputAction.CallbackContext context)
+	{
+		jump = true;
+	}
+}
