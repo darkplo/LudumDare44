91d133a1aa188234e8895b2f9d7c7e595253873b
Clément Cornut <cornut.clement@neuf.fr>
2019-04-28 10:35:17 +0200

    Menu + Intro

--- LD44/Assets/Scripts/MainMenuController.cs
+++ LD44/Assets/Scripts/MainMenuController.cs
@@ -10,20 +10,61 @@ public class MainMenuController : MonoBehaviour
     public GameObject creditMenu;
     public GameObject SettingsButton;
 
+    public GameObject background;
+    public GameObject backgroundPlay;
+    public GameObject backgroundCredits;
+    public GameObject backgroundQuit;
+    public GameObject backgroundBack;
+
+
     public void playGame() {
-        SceneManager.LoadScene("Hospital");
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void onPlayHover() {
+        background.SetActive(false);
+        backgroundPlay.SetActive(true);
+    }
+
+    public void OnPlayHoverLeave() {
+        background.SetActive(true);
+        backgroundPlay.SetActive(false);
+    }
+
+    public void onCreditsHover() {
+        background.SetActive(false);
+        backgroundCredits.SetActive(true);
+    }
+
+    public void OnCreditsHoverLeave() {
+        background.SetActive(true);
+        backgroundCredits.SetActive(false);
+    }
+
+    public void onQuitHover() {
+        background.SetActive(false);
+        backgroundQuit.SetActive(true);
+    }
+
+    public void OnQuitHoverLeave() {
+        background.SetActive(true);
+        backgroundQuit.SetActive(false);
     }
  
     public void settings() {
         mainMenu.SetActive(false);
+        background.SetActive(false);
         SettingsButton.SetActive(false);
         settingsMenu.SetActive(true);
+        backgroundBack.SetActive(true);
     }
 
     public void credits() {
         mainMenu.SetActive(false);
+        background.SetActive(false);
         SettingsButton.SetActive(false);
         creditMenu.SetActive(true);
+        backgroundBack.SetActive(true);
     }
 
     public void back() {
@@ -31,6 +72,9 @@ public class MainMenuController : MonoBehaviour
         SettingsButton.SetActive(true);
         creditMenu.SetActive(false);
         settingsMenu.SetActive(false);
+        backgroundBack.SetActive(false);
+        backgroundCredits.SetActive(false);
+        background.SetActive(true);
     }
  
     public void exitGame() {
---
+++ LD44/Assets/Scripts/introController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class introController : MonoBehaviour
+{
+    public GameObject image1;
+    public GameObject image2;
+
+    public void onImage1() {
+        image1.SetActive(false);
+        image2.SetActive(true);
+    }
+
+    public void onImage2() {
+        SceneManager.LoadScene("Hospital");
+    }
+}
